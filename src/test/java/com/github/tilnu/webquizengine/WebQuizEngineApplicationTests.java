6ce2877e9b3ed78c62ae57e3f9c5cb851b83708f
Ilnur <ilnur.abushakhmanov@yandex.ru>
2020-10-22 17:44:26 +0400

    Initial commit

---
+++ src/test/java/com/github/tilnu/webquizengine/WebQuizEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tilnu.webquizengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebQuizEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
